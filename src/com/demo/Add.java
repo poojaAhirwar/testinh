ebcbdccb143a79116a36059c777607b6ac3bdfd6
poojaAhirwar <you@example.com>
2022-11-10 04:22:16 -0800

    This is my first commit

---
+++ src/com/demo/Add.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter 2 numbers to add:");
+		int a= sc.nextInt();
+		int b= sc. nextInt();
+		int sum=a+b;
+		System.out.println("sum ="+ sum);
+		
+	}
+}
+
